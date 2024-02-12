8961267369b0d24f3fb5b31b2150f3e11eec8f35
shoaibisa <shoaibisa1@gmail.com>
2024-02-13 01:48:43 +0530

    added product

--- backend2/models/product.js
+++ backend2/models/product.js
@@ -72,6 +72,10 @@ const productSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
+    status: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     timestamps: true,
