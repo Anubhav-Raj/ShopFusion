060ab667c85aca3b611e18649fdd0c410ca71fed
shubham jha <116482692+shubhamjha2@users.noreply.github.com>
2024-02-26 00:20:49 +0530

    landing page updated

---
+++ frontend/src/components/Landing/Card.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Imgss from "./monitor-removebg-preview.png";
+
+function Card({ categories, onClick }) {
+  return (
+    <div className="categories segments snipcss-jPyl7">
+      <div className="container">
+        <div className="row">
+          {categories &&
+            categories.map((category, index) => (
+              <div key={index} className="col-20">
+                <div className="content" onClick={() => onClick(category.name)}>
+                  {/* Add onClick event to trigger handleDepartmentClick with category name */}
+                  <div className="icon">
+                    <img
+                      className="departimg"
+                      // src={category.image}
+                      src={Imgss}
+                      // alt={category.name}
+                    />
+                    <span className="topings">{category.name}</span>
+                  </div>
+                </div>
+              </div>
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
