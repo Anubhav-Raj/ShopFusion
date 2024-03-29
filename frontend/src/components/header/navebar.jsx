408cc21edc204ae4001ca888639915f2cbabc1a7
shubham jha <116482692+shubhamjha2@users.noreply.github.com>
2024-03-29 18:06:54 +0530

    small bugs fixed

--- frontend/src/components/header/navebar.jsx
+++ frontend/src/components/header/navebar.jsx
@@ -123,13 +123,14 @@ function Navbar() {
                     <div className="sm-menu">
                       <Link>
                         <svg className="icon" viewBox="0 0 24 24">
-                          <path d="M14,12L10,8V11H2V13H10V16M20,18V6C20,4.89 19.1,4 18,4H6A2,2 0 0,0 4,6V9H6V6H18V18H6V15H4V18A2,2 0 0,0 6,20H18A2,2 0 0,0 20,18Z"></path>
+                          <path d="M12,19.2C9.5,19.2 7.29,17.92 6,16C6.03,14 10,12.9 12,12.9C14,12.9 17.97,14 18,16C16.71,17.92 14.5,19.2 12,19.2M12,5A3,3 0 0,1 15,8A3,3 0 0,1 12,11A3,3 0 0,1 9,8A3,3 0 0,1 12,5M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12C22,6.47 17.5,2 12,2Z"></path>
                         </svg>
-                        <span className="text">Profie</span>
+
+                        <span className="text">Profile</span>
                       </Link>
                       <Link>
                         <svg className="icon" viewBox="0 0 24 24">
-                          <path d="M12,19.2C9.5,19.2 7.29,17.92 6,16C6.03,14 10,12.9 12,12.9C14,12.9 17.97,14 18,16C16.71,17.92 14.5,19.2 12,19.2M12,5A3,3 0 0,1 15,8A3,3 0 0,1 12,11A3,3 0 0,1 9,8A3,3 0 0,1 12,5M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12C22,6.47 17.5,2 12,2Z"></path>
+                          <path d="M14,12L10,8V11H2V13H10V16M20,18V6C20,4.89 19.1,4 18,4H6A2,2 0 0,0 4,6V9H6V6H18V18H6V15H4V18A2,2 0 0,0 6,20H18A2,2 0 0,0 20,18Z"></path>
                         </svg>
                         <span className="text" onClick={onLogoutHandler}>
                           logout
